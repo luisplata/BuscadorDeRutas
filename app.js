845f62d535081a2ca2306d957f4eea05c8d10546
Luis Plata <www.luisplata@gmail.com>
2018-10-07 11:37:01 -0500

    Se adiciona una parte para parametrizar la url

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var logger = require("./utilitys/logs").logger;
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', entorno.url_base+":"+entorno.puerto);
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
