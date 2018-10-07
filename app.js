00c9aef893ab5cd28e02ab2b1c3ee3736064432f
Luis Plata <www.luisplata@gmail.com>
2018-10-07 11:33:58 -0500

    Adicionando primera version para el cross-site

--- app.js
+++ app.js
@@ -5,6 +5,27 @@ var entorno = require('./utilitys/config-modules.js').config();
 var mysql = require("./utilitys/conexion");
 var logger = require("./utilitys/logs").logger;
 
+//middleware para una api publica
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 //configuracion
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
